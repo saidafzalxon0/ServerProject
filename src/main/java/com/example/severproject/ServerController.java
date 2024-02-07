de6d1d2f385f268e9301d6a0526033abf7fd9da0
Saidafzalkhon <Saidafzalkhon.developer@gmail.com>
2024-02-07 22:14:37 +0500

    salom

--- src/main/java/com/example/severproject/ServerController.java
+++ src/main/java/com/example/severproject/ServerController.java
@@ -1,2 +1,23 @@
-package com.example.severproject;public class ServerController {
+package com.example.severproject;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/enter")
+public class ServerController {
+
+    @GetMapping
+    public HashMap<String,String> convertMap(){
+        HashMap<String,String> map = new HashMap<>();
+        map.put("1","BIR");
+        map.put("2","IKKI");
+        map.put("3","UCH");
+        return map;
+    }
 }
